f10d791ea462382b39d2711117768c34542105d2
Chris Allen <cma@bitemyapp.com>
2013-07-30 21:53:14 -0700

    Orwellian

--- project.clj
+++ project.clj
@@ -7,6 +7,8 @@
                  [criterium "0.4.1" :scope "test"]
                  [joda-time "2.2"]
                  [commons-codec "1.6"]
+                 [sonian/carica "1.0.3" :exclusions [[cheshire]]]
+                 ;; http://en.wikipedia.org/wiki/Doublethink
                  [cheshire "5.2.0"]]
   :plugins [[lein-marginalia "0.7.1"]]
   :test-selectors {:default (complement :benchmark)
