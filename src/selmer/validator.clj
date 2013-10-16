7faaa70ff299da8c5f74ac6c4d7ff87b448eb6cb
Yogthos <dmitri.sotnikov@gmail.com>
2013-10-16 09:11:40 -0400

    added template in validation

--- src/selmer/validator.clj
+++ src/selmer/validator.clj
@@ -8,44 +8,52 @@
 (defn exception [& args]
   (throw ^java.lang.Exception (Exception. ^String (apply str args))))
 
-(defn validate-filters [line tag-value]
+(defn validate-filters [template line tag-value]
   (let [tag-filters (map
                   #(-> ^String % (.split ":") first keyword)
                   (-> tag-value name (.split "\\|") rest))]
     (if-not (empty? (difference (set tag-filters) (set (keys @filters))))
-      (exception "unrecognized filter for " tag-value " on line " line))))
+      (exception "Unrecognized filter for " tag-value " on line " line " for template " template))))
 
 (defn close-tags []
   (apply concat (vals @closing-tags)))
 
-(defn valide-tag [line tags {:keys [tag-name tag-value tag-type] :as tag}]
+(defn handle-closing-tags [tags tag-name]
+  (second (split-with #{tag-name} tags)))
+
+(handle-closing-tags [:else :foo :endif] :else)
+
+(defn valide-tag [template line tags {:keys [tag-name tag-value tag-type] :as tag}]
  (if (= :expr tag-type)
    (let [end-tags (get @closing-tags (-> tags last :tag-name))]
      (cond
        (nil? tag-name)
-       (exception "no tag name supplied for the tag on line " line)
+       (exception "nN tag name supplied for the tag on line " line " for template " template)
 
        (not-any? #{tag-name} (concat (close-tags) (keys @expr-tags)))
-       (exception "unrecognized tag: " (name tag-name) " on line " line)
+       (exception "Unrecognized tag: " (name tag-name) " on line " line " for template " template)
 
        (some #{tag-name} (close-tags))
        (let [tags (butlast tags)]
-         (if (= tag-name (last end-tags))
-           tags (conj tags (assoc tag :line line))))
+         (if
+           (some #{tag-name} end-tags)
+           (if (= tag-name (last end-tags))
+             tags (conj tags (assoc tag :line line)))
+           (exception "Orphaned closing tag " tag-name " on line " line " for template " template)))
 
        (get @closing-tags tag-name)
        (conj tags (assoc tag :line line))
 
        (some #{tag-name} (close-tags))
-       (exception "found an orphan closing tag " tag-name " on line " line)))
-   (do (validate-filters line tag-value) tags)))
+       (exception "Orphan closing tag " tag-name " on line " line " for template " template)))
+   (do (validate-filters template line tag-value) tags)))
 
 (defn validate-tags [template]
  (with-open [rdr (reader template)]
    (loop [tags [], ch (read-char rdr), line 1]
      (if ch
        (if (open-tag? ch rdr)
-         (recur (valide-tag line tags (read-tag-info rdr)) (read-char rdr) line)
+         (recur (valide-tag template line tags (read-tag-info rdr)) (read-char rdr) line)
          (recur tags (read-char rdr) (if (= \newline ch) (inc line) line)))
        tags))))
 
@@ -55,5 +63,8 @@
       (->> (validate-tags template)
            (map (fn [{:keys [tag-name line]}] (str "\n" tag-name " on line " line)))
            doall
-           (apply str "the template contains orphan tags: ")
+           (apply str "The template contains orphan tags: ")
            exception))))
+
+
+(validate (.getAbsolutePath (java.io.File. "test/templates/validation-test.html")))
