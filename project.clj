7e936b0084bc7a2663ac3017a9bfb82c72253b8e
Yogthos <dmitri.sotnikov@gmail.com>
2014-02-16 01:14:28 -0500

    bumped up dependencies

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
-(defproject selmer "0.5.9"
+(defproject selmer "0.6.1"
   :description "Django templates for Clojure"
   :url "https://github.com/yogthos/Selmer"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [criterium "0.4.2" :scope "test"]
+                 [criterium "0.4.3" :scope "test"]
                  [joda-time "2.3"]
-                 [commons-codec "1.8"]
-                 [cheshire "5.2.0"]]
+                 [commons-codec "1.9"]
+                 [cheshire "5.3.1"]]
   :repl-options {:port 10123}
   :plugins [[lein-marginalia "0.7.1"]]
   :test-selectors {:default (complement :benchmark)
