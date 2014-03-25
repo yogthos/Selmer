07b52f516ec09ea23488f5325317a817c603cd4c
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2014-03-25 13:50:33 -0400

    Update project.clj

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/yogthos/Selmer"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [criterium "0.4.3" :scope "test"]
                  [joda-time "2.3"]
                  [commons-codec "1.9"]
