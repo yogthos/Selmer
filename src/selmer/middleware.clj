403f668d74c99f174e7709929171710ba7cda5d7
Yogthos <dmitri.sotnikov@gmail.com>
2014-06-16 07:20:39 -0400

    added dev middleware

---
+++ src/selmer/middleware.clj
@@ -0,0 +1,17 @@
+(ns selmer.middleware
+  (:require [selmer.parser :as parser]))
+
+(defn template-error-page
+  "development middleware for rendering a friendly error page when a parsing error occurs"
+  [handler dev?]
+  (if dev?
+    (fn [request]
+      (try
+        (handler request)
+        (catch clojure.lang.ExceptionInfo ex
+          (let [{:keys [type error-template] :as data} (ex-data ex)]
+            (if (= :selmer-validation-error type)
+              {:status 500
+               :body (parser/render error-template data)}
+              (throw ex))))))
+    handler))
