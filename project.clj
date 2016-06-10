de7e4b1083e46be4b9ab2df4c4ceab591efe5514
Andrea Richiardi <a.richiardi.work@gmail.com>
2016-06-09 17:24:16 -0700

    Change org.clojure/clojure to "provided"
    This way it won't be imported as transitive dependency anymore.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/yogthos/Selmer"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.7.0" :scope "provided"]
                  [criterium "0.4.4" :scope "test"]
                  [joda-time "2.9.3"]
                  [commons-codec "1.10"]
