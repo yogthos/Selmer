aa7b537999c4b027300d15792717fc422086ee78
dmitri.sotnikov@gmail.com <dmitri.sotnikov@gmail.com>
2018-10-28 10:16:47 -0400

    bumped up version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject selmer "1.12.2"
+(defproject selmer "1.12.3"
   :description "Django style templates for Clojure"
   :url "https://github.com/yogthos/Selmer"
   :license {:name "Eclipse Public License"
