6696c904d2314240d35dbb10fdb7070ae211422f
Viktor <viktor@medino.com>
2019-12-03 17:50:51 +0000

    Make the with tag support = sign in quoted variables.
    For example,
    
    ```
    (render "{% with math=\"1+1=1\" %}{{ math }}{% endwith %}" {})
    ```
    
    used to throw an error, but now it should be fine.

--- src/selmer/tags.clj
+++ src/selmer/tags.clj
@@ -263,8 +263,10 @@
 (defn with-handler [args tag-content render rdr]
   (let [content (get-in (tag-content rdr :with :endwith) [:with :content])
         args    (->> args
-                     (mapcat #(.split ^String % "="))
-                     (remove #{"="})
+                     ; Split on = signs, but preserving those = signs that occur inside quotes.
+                     ; Inspiration from https://stackoverflow.com/a/24450127/1568714
+                     (mapcat #(re-seq #"\"[^\"]*\"|[^= ]+" %))
+                     (remove nil?)
                      (compile-args))]
     (fn [context-map]
       (render content
