aef8237bf36669f7da39cc3c49de0f68dfd70ee9
dmitri.sotnikov@gmail.com <dmitri.sotnikov@gmail.com>
2020-09-28 12:31:28 -0400

    updated dependencies

--- project.clj
+++ project.clj
@@ -4,10 +4,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [commons-codec "1.11"]
+                 [commons-codec "1.15"]
                  [json-html "0.4.7"]
-                 [cheshire "5.8.1"]
-                 [criterium "0.4.4" :scope "test"]]
+                 [cheshire "5.10.0"]
+                 [criterium "0.4.6" :scope "test"]]
 
   :aot [selmer.node]
   :javac-options ["-target" "1.6"]
@@ -18,4 +18,4 @@
   :profiles {:dev {:global-vars {*warn-on-reflection* true}
                    :repl-options {:port 10123}
                    :plugins [[lein-marginalia "0.9.0"]]
-                   :dependencies [[environ "1.1.0"]]}})
+                   :dependencies [[environ "1.2.0"]]}})
