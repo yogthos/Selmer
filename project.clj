692a97d44140f9a19f4fddb28c9f62a34c3c6194
dmitri.sotnikov@gmail.com <dmitri.sotnikov@gmail.com>
2022-07-08 09:11:24 -0400

    bumped up version for release

--- project.clj
+++ project.clj
@@ -1,17 +1,13 @@
-(defproject selmer "1.12.51"
+(defproject selmer "1.12.53"
   :description "Django style templates for Clojure"
   :url "https://github.com/yogthos/Selmer"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [;; minimal clojure version
                  [org.clojure/clojure "1.9.0"]
-
                  ;; optional dependencies
                  [json-html "0.4.7" :scope "provided"]
-                 [cheshire "5.11.0" :scope "provided"]
-
-                 ;; test dependencies
-                 [criterium "0.4.6" :scope "test"]]
+                 [cheshire "5.11.0" :scope "provided"]]
 
   :aot [selmer.node]
   :javac-options ["-target" "1.6"]
@@ -24,4 +20,6 @@
                    :source-paths ["src" "dev"]
                    :plugins [[lein-marginalia "0.9.0"]]
                    :dependencies [[environ "1.2.0"]
-                                  [org.clojure/tools.namespace "1.3.0"]]}})
+                                  [org.clojure/tools.namespace "1.3.0"]
+                                  [criterium "0.4.6"]]}
+             :test {:dependencies [[criterium "0.4.6"]]}})
